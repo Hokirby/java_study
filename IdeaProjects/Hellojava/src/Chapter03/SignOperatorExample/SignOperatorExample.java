87cd4f46bb5fa1a988cdaa4c5ff7226e77a0d293
HoKirby <lakevely27@gmail.com>
2024-09-25 20:38:53 +0900

    java study Chapter03

---
+++ IdeaProjects/Hellojava/src/Chapter03/SignOperatorExample/SignOperatorExample.java
@@ -0,0 +1,12 @@
+package Chapter03.SignOperatorExample;
+
+public class SignOperatorExample {
+    public static void main(String[] args) {
+        int x = -100;
+        System.out.println("x: " + x);
+
+        byte b = 100;
+        int y = -b;
+        System.out.println("y: " + y);
+    }
+}
