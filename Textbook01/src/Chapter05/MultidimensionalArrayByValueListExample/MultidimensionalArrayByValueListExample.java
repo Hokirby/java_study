755aafdc1e15a0aa198aecf617b492fb4f989ef2
HoKirby <lakevely27@gmail.com>
2024-09-29 22:58:03 +0900

    java study Chapter05

---
+++ Textbook01/src/Chapter05/MultidimensionalArrayByValueListExample/MultidimensionalArrayByValueListExample.java
@@ -0,0 +1,14 @@
+package Chapter05.MultidimensionalArrayByValueListExample;
+
+public class MultidimensionalArrayByValueListExample {
+    public static void main(String[] args) {
+        //2차원 배열 생성
+        int[][] scores = {
+                { 80, 90, 96},
+                { 76, 88 }
+        };
+
+        //배열의 길이
+        System.out.println("1차원 배열 길이(반의 수): " + scores.length);
+    }
+}
