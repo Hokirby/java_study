de2877d274bcd0bb8b96ba5b5a99878de609f176
HoKirby <lakevely27@gmail.com>
2024-10-04 18:14:59 +0900

    java study Chapter06

---
+++ Textbook01/src/Chapter06/Car/Car.java
@@ -0,0 +1,28 @@
+package Chapter06.Car;
+
+public class Car {
+    //필드 선언
+    String  company = "현대자동차";
+    String model;
+    String color;
+    boolean start;
+    int maxSpeed;
+    int speed;
+
+    //생성자 선언
+    Car() {}
+
+    Car(String model) {
+        this(model, "은색", 250);
+    }
+
+    Car(String model, String color) {
+        this(model, color, 250);
+    }
+
+    Car(String model, String color, int maxSpeed) {
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+}
---
+++ Textbook01/src/Chapter06/Car1/Car1Example.java
@@ -0,0 +1,9 @@
+package Chapter06.Car1;
+
+public class Car1Example {
+    public static void main(String[] args) {
+        Car1 myCar = new Car1("그랜저", "검정", 250);
+        //Car1 myCar = new Car1(); //기본 생성자 호출 못함
+
+    }
+}
