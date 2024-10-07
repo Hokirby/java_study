5e06e2111e823d3b779c6e5a4c87a47dbdf5e94a
HoKirby <lakevely27@gmail.com>
2024-10-07 14:03:55 +0900

    java study Chapter06 exercise

--- Textbook01/src/Chapter06/Exercise/Account.java
+++ Textbook01/src/Chapter06/Exercise/Account.java
@@ -4,15 +4,33 @@ public class Account {
     String accountNum;
     String name;
     int money;
+    String[] accountArray;
 
-    public String[] accountMake (String accountNum, String name, int money) {
+    Account(String accountNum, String name, int money) {
         this.accountNum = accountNum;
         this.name = name;
         this.money = money;
+    }
+
+    public String[] accountMake() {
         String[] accountMade = new String[3];
-        accountMade[0]= accountNum;
-        accountMade[1] = name;
-        accountMade[2] = String.valueOf(money);
+        accountMade[0] = this.accountNum;
+        accountMade[1] = this.name;
+        accountMade[2] = String.valueOf(this.money);
         return accountMade;
     }
+
+    public static int accountInfoMake(String[][] accountArray, String userAccount) {
+        int i = 0;
+        int accountInfo;
+        while (i <= accountArray.length) {
+            if (accountArray[i][0].equals(userAccount)) {
+                accountInfo = i;
+                break;
+            }else i++;
+        }
+        return i;
+    }
+
+
 }
--- Textbook01/src/Chapter06/Exercise/BankApplication.java
+++ Textbook01/src/Chapter06/Exercise/BankApplication.java
@@ -7,13 +7,15 @@ public class BankApplication {
         Scanner scanner = new Scanner(System.in);
         String[][] accountArray = new String[100][3];
         int num = 0;
+        int accountInfo;
 
         while(true) {
             System.out.println("-----------------------------");
-            System.out.println("1.계좌생성| 2.계좌목록|3.예금|4.출금|5.종료");
+            System.out.println("1.계좌생성|2.계좌목록|3.예금|4.출금|5.종료");
             System.out.println("-----------------------------");
             System.out.print("선택> ");
             String userInput = scanner.nextLine();
+
             if (userInput.equals("1")) {
                 System.out.println("-----------------------------");
                 num += 1;
@@ -23,18 +25,15 @@ public class BankApplication {
                 String name = scanner.nextLine();
                 System.out.print("초기입금액: ");
                 int money = scanner.nextInt();
-
-                Account account = new Account();
-                String[] accountMade = new String[3];
-                accountMade = account.accountMake(accountNum, name, money);
-                System.arraycopy(accountArray[num-1],num-1, accountMade, 0,  accountMade.length);
+                Account account = new Account(accountNum, name, money);
+                accountArray[num-1] = account.accountMake();
                 System.out.println("결과: 계좌가 생성되었습니다.");
             } else if (userInput.equals("2")) {
                 System.out.println("-----------------------------");
                 System.out.println("계좌목록");
                 System.out.println("-----------------------------");
-                for (int i = 0; i <= num; i++) {
-                    System.out.println(accountArray[i][0] + "\t" + accountArray[i][1] + "\t"+ accountArray[i][2]);
+                for (int i = 0; i <= num-1; i++) {
+                    System.out.println(accountArray[i][0] + "\t" + accountArray[i][1] + "\t" + accountArray[i][2]);
                 }
             } else if (userInput.equals("3")) {
                 System.out.println("-----------------------------");
@@ -42,21 +41,24 @@ public class BankApplication {
                 System.out.println("-----------------------------");
                 System.out.print("계좌번호: ");
                 String userAccount = scanner.nextLine();
-                System.out.println("예금액: ");
+                accountInfo = Account.accountInfoMake(accountArray, userAccount);
+                System.out.print("예금액: ");
                 int deposit = scanner.nextInt();
-
+                accountArray[accountInfo][2] = String.valueOf(Integer.parseInt(accountArray[accountInfo][2]) + deposit);
             } else if (userInput.equals("4")) {
                 System.out.println("-----------------------------");
                 System.out.println("출금");
                 System.out.println("-----------------------------");
                 System.out.print("계좌번호: ");
                 String userAccount = scanner.nextLine();
-                System.out.println("출금액: ");
+                System.out.print("출금액: ");
                 int withdraw = scanner.nextInt();
+                accountInfo = Account.accountInfoMake(accountArray, userAccount);
+                accountArray[accountInfo][2] = String.valueOf(Integer.parseInt(accountArray[accountInfo][2]) - withdraw);
                 System.out.println("출금이 성공되었습니다.");
-
             } else if (userInput.equals("5")) {
                 System.out.println("프로그램 종료");
+                break;
             }
 
         }
